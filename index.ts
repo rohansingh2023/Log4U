29f36871dd6447fcaa132b0346f26c322cccbd10
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2024-11-30 14:53:03 +0530

    added files

--- index.ts
+++ index.ts
@@ -1,1 +1,41 @@
-export * from './logging/index'
+import Logger from "./logging";
+
+const logger = new Logger({ serviceName: "TEST"});
+
+logger.log({
+  type: "INFO",
+  message: "Testing my logs package",
+});
+
+logger.log({
+  type: "DEBUG",
+  message: "Test with other payloads",
+  payload: { id: 123344 },
+});
+
+logger.log({
+  type: "DEBUG",
+  message: "Test with other payloads",
+  statusCode: 200,
+});
+
+logger.log({
+  type: "DEBUG",
+  message: "Test with other payloads",
+  targetURL: "http://localhost:5173",
+});
+
+try {
+    throw new Error("This is a test error");
+} catch (err) {
+    logger.log({
+        type: "ERROR",
+        logId: "ERR001",
+        message: err, // Pass the actual Error object
+    });
+}
+
+logger.log({
+    type: "INFO",
+    message: "Ending my logs package",
+});
